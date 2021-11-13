950c4f81025ee641f6a5650ee63918034e6e6e89
Ricardo Altmann <ricardoaltmann@outlook.com.br>
2021-11-13 18:32:03 -0300

    comentarios e final do teste modelo 1

--- COPA.php
+++ COPA.php
@@ -1,7 +1,7 @@
 <?php
 
-$inicio = new DateTime('2022-11-21');
-$intervalo = new DateInterval('P4Y');
+$inicio = new DateTime('2022-11-21'); // Data inicial para a contagem do intervalo
+$intervalo = new DateInterval('P4Y'); //P de periodo e 4 para contar 4 anos e Y significa ano
 $periodo = new DatePeriod($inicio, $intervalo, '4');
 
 foreach ($periodo as $data) {
